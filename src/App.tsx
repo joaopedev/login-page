28f68dca2adb8a7d1836ca477055197b4576f076
joaopedev <joaopedev@outlook.com>
2022-06-09 16:53:48 -0300

    changes in home

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,12 @@
-import * as React from "react"
-import {
-  Button,
-  ButtonGroup,
-  IconButton,
-  Box,
-  ChakraProvider,
-  Grid,
-  GridItem,
-  Input,
-  Text,
-  theme,
-  VStack,
-  InputGroup,
-  InputRightElement,
-  Spacer,
-  Flex,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { SearchIcon, LockIcon } from '@chakra-ui/icons'
-
+import { ChakraProvider } from "@chakra-ui/react";
+import * as React from "react";
+import { Router, Route, Link } from "react-router-dom";
+import { LoginBox } from "./components/home/homepage";
 
 export const App = () => {
-  const [show, setShow] = React.useState(false)
-  const handleClick = () => setShow(!show)
   return (
-
-
-    <ChakraProvider theme={theme}>
-      <Grid templateColumns="1fr 480px 480px 1fr" gap={5} minH="100vh" p={3}>
-        <GridItem w='100%' h='100%'></GridItem>
-        <GridItem>
-        </GridItem>
-        <GridItem display='grid' alignContent='center' w='100%' h="100%">
-          <Box textAlign="right" fontSize="xl" boxShadow='lg' p='6' rounded='md'>
-            <VStack spacing={8}>
-              <Box display='flex' justifyContent='space-between'>
-              <LockIcon w='100px' h='100px'></LockIcon> 
-              <Text p={10} display='flex' alignItems='center'>
-                My login page
-              </Text>
-              </Box>
-              <InputGroup>
-                <Input placeholder="Email" />
-              </InputGroup>
-              <InputGroup>
-                <Input type={show ? 'text' : 'password'} placeholder="Password" />
-                <InputRightElement width='4.5rem'>
-                  <Button h='1.75rem' size='sm' onClick={handleClick}>
-                    {show ? 'Hide' : 'Show'}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-              <Box display='flex'  pl='20%' pr='20%' justifyContent='space-between' w='100%'>
-              <Button>Enter</Button>
-              <Button>Register</Button>
-              </Box>
-            </VStack>
-          </Box>
-        </GridItem>
-        <GridItem display='flex' justifyContent='flex-end' w='100%' h='100%'>
-          <ColorModeSwitcher boxShadow='lg' p='6' rounded='md' />
-        </GridItem>
-      </Grid>
+    <ChakraProvider>
+      <LoginBox/>
     </ChakraProvider>
-  )
-}
+  );
+};
---
+++ src/components/home/homepage.tsx
@@ -0,0 +1,90 @@
+import * as React from "react";
+import { useState } from "react";
+import {
+  Button,
+  Box,
+  ChakraProvider,
+  Grid,
+  GridItem,
+  Input,
+  Text,
+  theme,
+  VStack,
+  InputGroup,
+  InputRightElement,
+  Image,
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "../../ColorModeSwitcher";
+import { LockIcon } from "@chakra-ui/icons";
+import { DotaBlack } from "./dotablack";
+import { DotaWhite } from "./dotawhite";
+
+export const LoginBox = () => {
+  const [show, setShow] = useState(false);
+  const [trade, setTrade] = useState(false);
+  const handleClick = () => setShow(!show);
+  const handleDota = () => setTrade(!trade);
+  return (
+    <ChakraProvider theme={theme}>
+      <Grid templateColumns="1fr 480px 480px 1fr" gap={5} minH="100vh" p={3}>
+        <GridItem w="100%" h="100%"></GridItem>
+        <GridItem display="grid" justifyContent="start" alignContent="center" w="100%" h="100%">
+          <VStack flexDirection="row" spacing={8}>
+            <Box>
+              <DotaWhite />
+            </Box>
+            <Box>
+              <Text>Hello, on this site you will be able to become a friend of Jassa and invite him to play with you.</Text>
+              {/* font-family: "Radiance","Noto Sans",sans-serif; */}
+            </Box>
+          </VStack>
+        </GridItem>
+        <GridItem display="grid" alignContent="center" w="100%" h="100%">
+          <Box
+            textAlign="right"
+            fontSize="xl"
+            boxShadow="lg"
+            p="6"
+            rounded="md"
+          >
+            <VStack spacing={8}>
+              <Box display="flex" justifyContent="space-between">
+                <LockIcon w="100px" h="100px"></LockIcon>
+                <Text p={10} display="flex" alignItems="center">
+                  Jassa Friends
+                </Text>
+              </Box>
+              <InputGroup>
+                <Input placeholder="Email" />
+              </InputGroup>
+              <InputGroup>
+                <Input
+                  type={show ? "text" : "password"}
+                  placeholder="Password"
+                />
+                <InputRightElement width="4.5rem">
+                  <Button h="1.75rem" size="sm" onClick={handleClick}>
+                    {show ? "Hide" : "Show"}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+              <Box
+                display="flex"
+                pl="20%"
+                pr="20%"
+                justifyContent="space-between"
+                w="100%"
+              >
+                <Button>Enter</Button>
+                <Button>Register</Button>
+              </Box>
+            </VStack>
+          </Box>
+        </GridItem>
+        <GridItem display="flex" justifyContent="flex-end" w="100%" h="100%">
+          <ColorModeSwitcher boxShadow="lg" p="6" rounded="md" />
+        </GridItem>
+      </Grid>
+    </ChakraProvider>
+  );
+};
