1124bef2b2aef05ac68d307ed6077d6c6a7fb6f9
joaopedev <joaopedev@outlook.com>
2022-07-27 20:00:49 -0300

    form constructor

--- src/components/home/homepage.tsx
+++ src/components/home/homepage.tsx
@@ -12,27 +12,37 @@ import {
   VStack,
   InputGroup,
   InputRightElement,
+  FormControl,
+  FormHelperText,
   Image,
 } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "../../ColorModeSwitcher";
 import { LockIcon } from "@chakra-ui/icons";
 import { DotaBlack } from "./dotablack";
 import { DotaWhite } from "./dotawhite";
+import { SignupForm } from "../form/form"
 
 export const LoginBox = () => {
   const [show, setShow] = useState(false);
   const [trade, setTrade] = useState(false);
   const handleClick = () => setShow(!show);
-  const handleDota = () => setTrade(!trade);
   return (
     <ChakraProvider theme={theme}>
       <Grid templateColumns="1fr 480px 480px 1fr" gap={5} minH="100vh" p={3}>
-        <GridItem w="100%" h="100%">
-        </GridItem>
-        <GridItem display="grid" justifyContent="start" alignContent="center" w="100%" h="100%">
+        <GridItem w="100%" h="100%"></GridItem>
+        <GridItem
+          display="grid"
+          justifyContent="start"
+          alignContent="center"
+          w="100%"
+          h="100%"
+        >
           <VStack flexDirection="row" spacing={8}>
             <Box>
-              <Text display="flex" alignItems="center">Hello, on this site you will be able to become a friend of Jassa and invite him to play with you.</Text>
+              {/* <Text display="flex" alignItems="center">
+                Hello, on this site you will be able to become a friend of Jassa
+                and invite him to play with you.
+              </Text> */}
               {/* font-family: "Radiance","Noto Sans",sans-serif; */}
             </Box>
           </VStack>
@@ -48,16 +58,17 @@ export const LoginBox = () => {
             <VStack spacing={8}>
               <Box display="flex" justifyContent="space-between">
                 {/* <LockIcon w="100px" h="100px"></LockIcon> */}
-                <DotaBlack/>
+                <DotaWhite />
                 <Text p={10} display="flex" alignItems="center">
                   Jassa Friends
                 </Text>
               </Box>
-              <InputGroup>
-                <Input placeholder="Email" />
-              </InputGroup>
+              <FormControl>
+                <Input placeholder="exemple@gmail.com" type="email" />
+                <FormHelperText p={2}>We'll never share your email.</FormHelperText>
               <InputGroup>
                 <Input
+                  p={2}
                   type={show ? "text" : "password"}
                   placeholder="Password"
                 />
@@ -67,6 +78,7 @@ export const LoginBox = () => {
                   </Button>
                 </InputRightElement>
               </InputGroup>
+              </FormControl>
               <Box
                 display="flex"
                 pl="20%"
